0568c6485ff4e872ea875dc48a06554a4b61c269
zaoldyeck <zaoldyeck9970@gmail.com>
2026-07-14 20:24:25 +0800

    Initial commit: Set up premium Astro blog with Cal.com booking & Stripe payment

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'DevInsight';
+export const SITE_DESCRIPTION = '分享軟體開發技術、職涯成長與線上諮詢服務的個人部落格';
